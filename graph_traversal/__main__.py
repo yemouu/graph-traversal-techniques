160b29709cf2508be52b1b68b3dada5929fa843f
yemou <dev@lilac.pink>
2024-10-31 22:44:15 -0400

    Add more graph examples

--- graph_traversal/__main__.py
+++ graph_traversal/__main__.py
@@ -2,7 +2,23 @@ from .methods import bfs_list, dfs_list
 
 
 def main():
-    adacency_list: dict[str, tuple[str]] = {
+    adacency_list1: dict[str, tuple[str]] = {
+        "A": ("B", "D"),
+        "B": ("E",),
+        "C": ("D",),
+        "D": ("A", "C"),
+        "E": ("B",),
+    }
+
+    adacency_list2: dict[str, tuple[str]] = {
+        "A": ("C",),
+        "B": ("C", "D"),
+        "C": ("A", "B"),
+        "D": ("B",),
+        "E": ("D",),
+    }
+
+    adacency_list3: dict[str, tuple[str]] = {
         "A": ("B", "C"),
         "B": ("A", "D", "E"),
         "C": ("A", "F", "G"),
@@ -12,9 +28,13 @@ def main():
         "G": ("C",),
     }
 
-    print(bfs_list(adacency_list, "A"))
+    print(bfs_list(adacency_list1, "A"))
+    print(bfs_list(adacency_list2, "A"))
+    print(bfs_list(adacency_list3, "A"))
 
-    print(dfs_list(adacency_list, "A"))
+    print(dfs_list(adacency_list1, "A"))
+    print(dfs_list(adacency_list2, "A"))
+    print(dfs_list(adacency_list3, "A"))
 
 
 if __name__ == "__main__":
